0488ee353de5f6a7b4e4296546a97103c918ea1a
Jean-Paul Mosquera Arevalo <jpmosquera1@utpl.edu.ec>
2018-06-29 14:39:44 -0500

    update

---
+++ pages/VisGraph/javascript/script1.js
@@ -0,0 +1,29 @@
+
+        // define config car
+        // instantiate nodevis object
+        // draw
+        var viz;
+        function draw() {
+            var config = {
+                container_id: "container",
+                server_url: "bolt://18.207.142.251:34087",
+                server_user: "neo4j",
+                server_password: "trial-lumber-mirror",
+                labels: {
+                    //"Character": "name",
+                    "Personaje": {
+                        "Nombre": name,
+                        //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+                    }
+                },
+                relationships: {
+                    "NACIO_EN": {
+                        "Pais": "Country",
+                        "Ciudad": "Name"
+                    }
+                },
+            };
+            viz = new NeoVis.default(config);
+            viz.render();
+            console.log(viz);
+        }
